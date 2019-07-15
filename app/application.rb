8ff08ac601f6247821dc222e886a262e33f9ef67
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-15 02:58:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,14 @@
 class Application
 
   def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+      item = req.params[""]
+      resp.write item.price
+    else
+      
   end
 
 end
